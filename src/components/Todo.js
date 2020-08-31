f0581537c271691b5dffb1ec91e160d2085a136c
Erica Girges <ericafiorenza@gmail.com>
2020-08-31 15:45:52 -0700

    just need clear button functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledTask = styled.div `
+    font-family: "Raleway";
+    font-size: 1.3em;
+    margin-top: 20px;
+    cursor: pointer;
+
+    :hover {
+        color: #68487F;
+    }
+
+    &.taskcompleted {
+        color: #68487F;
+        font-style: italic;
+        text-decoration: line-through;
+    }
+`
 
 const Todo = (props) => {
   return (
-    <div
+    <StyledTask
       onClick={() => props.toggleCompleted(props.task.id)}
-      className={`task${props.task.completed ? " completed" : ""}`}
+      className={`task${props.task.completed ? "completed" : ""}`}
     >
       <p>{props.task.name}</p>
-    </div>
+    </StyledTask>
   );
 };
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,32 @@
 // feel free to change this component.js into TodoList.js
 import React from "react";
 import Todo from "./Todo";
+import styled from "styled-components";
+
+const TasksContainer = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const ClearButton = styled.button `
+    font-family: "Raleway";
+    background: none;
+    border: none;
+    font-weight: 700;
+    font-size: 1.4em;
+    margin-top: 30px;
+
+    :hover {
+        color: #68487F; 
+    }
+
+`
 
 const TodoList = (props) => {
 
   return (
-    <div>
+    <TasksContainer>
       {props.thingsTodo.map((task) => (
         <Todo
           key={task.id}
@@ -14,8 +35,8 @@ const TodoList = (props) => {
           toggleCompleted={props.toggleCompleted}
         />
       ))}
-      <button>Clear Completed Tasks</button>
-    </div>
+      <ClearButton>Clear Completed Tasks</ClearButton>
+    </TasksContainer>
   );
 };
 
